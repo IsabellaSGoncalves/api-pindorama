4ccf96ae2db0eda783a88386a8b7acf6ecee2bb1
Isabella da Silva Gonçalves <161075154+IsabellaSGoncalves@users.noreply.github.com>
2025-09-13 20:16:51 -0300

    :bricks: ci: permitindo o host para deploy no render

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,6 @@ Rails.application.configure do
   #
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+  config.hosts << "api-pindorama.onrender.com"
+
 end
