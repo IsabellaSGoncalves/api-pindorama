21eed7d805cb42d6635b8cc64f4e13a9dd4b5d39
AnahiMamani <narielanahi@gmail.com>
2025-09-19 23:18:57 -0300

    :wrench: Adicionando models, migrations e controllers para as tabelas autoor, artigos e eventos

---
+++ app/models/autor.rb
@@ -0,0 +1,6 @@
+#Definindo as cardinalidades/relacionamentos entre tabelas
+#dependent: :nullify se o autor for deletado, artigos ficam com autor_id = NULL
+class Autor < ApplicationRecord
+  has_many :artigos, dependent: :nullify
+  has_many :eventos, dependent: :nullify
+end
---
+++ test/controllers/artigos_controller_test.rb
@@ -0,0 +1,38 @@
+require "test_helper"
+
+class ArtigosControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @artigo = artigos(:one)
+  end
+
+  test "should get index" do
+    get artigos_url, as: :json
+    assert_response :success
+  end
+
+  test "should create artigo" do
+    assert_difference("Artigo.count") do
+      post artigos_url, params: { artigo: { autor_id: @artigo.autor_id, conteudo: @artigo.conteudo, data: @artigo.data, local: @artigo.local, tags: @artigo.tags, titulo: @artigo.titulo, url_imagem: @artigo.url_imagem } }, as: :json
+    end
+
+    assert_response :created
+  end
+
+  test "should show artigo" do
+    get artigo_url(@artigo), as: :json
+    assert_response :success
+  end
+
+  test "should update artigo" do
+    patch artigo_url(@artigo), params: { artigo: { autor_id: @artigo.autor_id, conteudo: @artigo.conteudo, data: @artigo.data, local: @artigo.local, tags: @artigo.tags, titulo: @artigo.titulo, url_imagem: @artigo.url_imagem } }, as: :json
+    assert_response :success
+  end
+
+  test "should destroy artigo" do
+    assert_difference("Artigo.count", -1) do
+      delete artigo_url(@artigo), as: :json
+    end
+
+    assert_response :no_content
+  end
+end
