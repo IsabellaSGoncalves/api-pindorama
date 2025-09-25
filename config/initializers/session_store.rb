c0cee8605f7f6272541f825df50432c126c5ad6d
Isabella da Silva Gonçalves <161075154+IsabellaSGoncalves@users.noreply.github.com>
2025-09-25 19:26:29 -0300

    :bug: fix: configurando corretamente cookie

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,11 +1,13 @@
-if Rails.env.development?
-  Rails.application.config.session_store :cookie_store,
-    key: "_pindorama_session",
-    same_site: :lax
-else
+  # if Rails.env.production
+  #   Rails.application.config.session_store :cookie_store,
+  #     key: "_pindorama_session",
+  #     same_site: :lax
+  # else
+  # para teste em produção ^^^
+
   Rails.application.config.session_store :cookie_store,
     key: "_pindorama_session",
     same_site: :none,
     secure: true,
     expire_after: 4.hours
-end
+# end
