42c1d2a4c0260882ece25e434f41e8d74ce7ccfb
Isabella da Silva Gonçalves <161075154+IsabellaSGoncalves@users.noreply.github.com>
2025-09-25 21:29:36 -0300

    fix: cookie

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,6 +69,8 @@ Rails.application.configure do
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
 
+  config.force_ssl = true
+
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
