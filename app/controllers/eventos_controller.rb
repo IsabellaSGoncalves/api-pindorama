4d82c0e08d8a11e0e036a472cb764a203e3e1c3f
Amanda <amanda.s.brito@hotmail.com>
2025-09-26 17:04:56 -0300

    /

--- app/controllers/eventos_controller.rb
+++ app/controllers/eventos_controller.rb
@@ -14,21 +14,15 @@ class EventosController < ApplicationController
 
   # POST /eventos
   def create
-    # mantive o imagem_capa porque é assim que é enviado do hook
     uploaded_image = params[:evento][:imagem_capa]
 
     image_url = nil
     if uploaded_image.present?
-      # copiei na cara dura da Anahí esse trecho da imagem para o Cloudinary, incluindo a URL da imagem, é armazenado na variável `result`.
       result = Cloudinary::Uploader.upload(uploaded_image)
-      
-      # Extrai a URL do resultado do upload.
       image_url = result["secure_url"]
     end
 
-    # O `evento_params` pega os dados como título, conteúdo, tags, etc...
-    # Ajustado para usar urlImagemEvento: image_url
-    @evento = Evento.new(evento_params.merge(urlImagemEvento: image_url))
+    @evento = Evento.new(evento_params.merge(url_imagem: image_url))
 
     if @evento.save
       render json: @evento, status: :created, location: @evento
@@ -47,11 +41,9 @@ class EventosController < ApplicationController
       image_url = result["secure_url"]
     end
 
-    #parâmetros para a atualização.
     update_params = evento_params
     
-    # Ajustado para usar urlImagemEvento: image_url
-    update_params = update_params.merge(urlImagemEvento: image_url) if image_url.present?
+    update_params = update_params.merge(url_imagem: image_url) if image_url.present?
 
     if @evento.update(update_params)
       render json: @evento
@@ -66,22 +58,19 @@ class EventosController < ApplicationController
   end
 
   private
-    # O `set_evento` foi ajustado para `params[:id]`
     def set_evento
       @evento = Evento.find(params[:id])
     end
 
-    # - `:imagem_capa` é um parâmetro permitido para que possamos acessar pelo controller antes de subir 
-    # - `tagsEvento: []` recebe o array de tags
     def evento_params
       params.require(:evento).permit(
-        :tituloEvento, 
-        :conteudoEvento, 
-        :dataEvento, 
-        :localEvento, 
-        :autorEvento, 
+        :titulo, 
+        :conteudo, 
+        :data, 
+        :local, 
+        :autor_id, 
         :imagem_capa,
-        tagsEvento: []
+        tags: []
       )
     end
 end
