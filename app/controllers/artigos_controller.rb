b5e4e73e42e43557fe99ce2274c60f193ff6c51c
Anahi Narieli <115493470+AnahiMamani@users.noreply.github.com>
2025-12-03 04:16:30 -0300

    Add eager loading for artigo associations
    Optimize artigo retrieval by eager loading associated autor and imagem.

--- app/controllers/artigos_controller.rb
+++ app/controllers/artigos_controller.rb
@@ -150,7 +150,8 @@ class ArtigosController < ApplicationController
 private
     # Use callbacks to share common setup or constraints between actions.
     def set_artigo
-      @artigo = Artigo.find(params[:id])
+      # MUDANÇA AQUI: Adicionar .includes(:autor, :imagem)
+      @artigo = Artigo.includes(:autor, :imagem).find(params[:id])
     end
 
       # Only allow a list of trusted parameters through.
